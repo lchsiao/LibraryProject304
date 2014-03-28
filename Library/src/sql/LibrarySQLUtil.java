a271763edfb1eb23d7dda0828853710e3b55f9ff
Ziqin (Jimmy) Wang <jimmywangzq@gmail.com>
2014-03-28 14:41:44 -0700

    added addBorrower

--- Library/src/sql/LibrarySQLUtil.java
+++ Library/src/sql/LibrarySQLUtil.java
@@ -1,17 +1,19 @@
 package sql;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 
 public class LibrarySQLUtil {
-
+    
 	// db fields
 	private static final String CONNECT_URL = "jdbc:oracle:thin:@localhost:1521:ug";
-	private static final String USER = "..."; 
+	private static final String USER = "...";
 	private static final String PASSWORD = "...";
-
+    
 	private static Connection conn;
 	
 	// command strings
@@ -25,7 +27,7 @@ public class LibrarySQLUtil {
 	private LibrarySQLUtil(){}
 	
 	public static void loadDriver() {
-		try 
+		try
 		{
 			// Load the Oracle JDBC driver
 			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
@@ -35,9 +37,9 @@ public class LibrarySQLUtil {
 			System.out.println("Message: " + ex.getMessage());
 			System.exit(-1);
 		}
-
+        
 	}
-
+    
 	private static Connection getConnection()
 	{
 		try {
@@ -45,7 +47,7 @@ public class LibrarySQLUtil {
 				System.out.println("\nRetreiving existing connection");
 				return conn;
 			}
-
+            
 			conn = DriverManager.getConnection(CONNECT_URL, USER, PASSWORD);
 			System.out.println("\nNew connection established");
 			return conn;
@@ -57,12 +59,35 @@ public class LibrarySQLUtil {
 		
 		return null;
 	}
-
+    
 	public static String addBorrower(String name, String password, String address, String phone,
-			String email, String sinOrStdNo, String type) {
+                                     String email, String sinOrStdNo, String type) {
 		//TODO
-		// conn.prepareStatement... etc...
-		
+		try {
+			PreparedStatement ps = conn.prepareStatement("INSERT INTO borrower VALUES (?,?,?,?,?,?,?)");
+			ps.setString(1, name);
+			ps.setString(2, password);
+			ps.setString(3, address);
+			if (phone.length() == 0) {
+				ps.setNull(4, java.sql.Types.INTEGER);
+			} else {
+                int phonenum = Integer.parseInt(phone);
+                ps.setInt(4, phonenum);
+			}
+			ps.setString(5, email);
+			ps.setInt(6, Integer.parseInt(sinOrStdNo));
+			ps.setString(7, type);
+			ps.executeUpdate();
+		    conn.commit();
+			ps.close();
+		} catch (SQLException e) {
+			try {
+				System.out.println("SQLException: " + e.getMessage());
+				conn.rollback();
+			} catch (SQLException e1) {
+				System.out.println("SQLException on rollback: " + e1.getMessage());
+			}
+		}
 		return SUCCESS_STRING;
 	}
 	
